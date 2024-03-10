d34b8be50cd4e4be6471a1caa0115aabd6949ba0
Marcel W. Wysocki <maci.stgn@gmail.com>
2024-03-10 21:56:10 +0800

    Remove build.sh and ui.py files

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#gcc -std=c99 -pedantic-errors -Werror -Wall -pedantic -O3 -o interpreter interpreter.c
-gcc -std=c99 -pedantic-errors -Werror -Wall -pedantic -g -o interpreter interpreter.c
